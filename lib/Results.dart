bd6a6399c50ad8d15f7128efb5dc6aa3337f0848
Timothy <wng.timothy@gmail.com>
2021-04-10 18:56:35 -0500

    Results UI without confetti

--- lib/Results.dart
+++ lib/Results.dart
@@ -19,205 +19,167 @@ class _ResultsState extends State<Results> {
 
   bool gain = true;
 
-  ConfettiController _controllerTopCenter;
-  @override
-  void initState() {
-    _controllerTopCenter =
-        ConfettiController(duration: const Duration(seconds: 10));
-    super.initState();
-  }
-
-  void dispose() {
-    _controllerTopCenter.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
 
-        body: Stack(
-          children: [
-            Align(
-              alignment: Alignment.topCenter,
-              child: ConfettiWidget(
-                confettiController: _controllerTopCenter,
-                blastDirection: pi / 2,
-                maxBlastForce: 5, // set a lower max blast force
-                minBlastForce: 2, // set a lower min blast force
-                emissionFrequency: 0.05,
-                numberOfParticles: 50, // a lot of particles at once
-                gravity: 1,
+              // == SPACE ==
+              Container(
+                height: 50,
               ),
-            ),
-            Align(
-              alignment: Alignment.topCenter,
-              child: FlatButton(
-                  onPressed: () {
-                    _controllerTopCenter.play();
-                  },
+
+              // == PLACEMENT ==
+              Container(
+                height: 100,
+                width: 200,
+                decoration: BoxDecoration(
+                  color: Colors.orangeAccent,
+                  border: Border.all(color: Colors.black38),
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      offset: const Offset(
+                        3.0,
+                        3.0,
+                      ),
+                      blurRadius: 5,
+                      spreadRadius: 2,
+                    ),
+                    BoxShadow(
+                      color: Colors.white,
+                      offset: const Offset(0.0, 0.0),
+                      blurRadius: 0.0,
+                      spreadRadius: 0.0,
+                    ),
+                  ],
+                ),
+                child: Center(
+                  child: Text("$placement/$total",
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 46),
                   ),
-            ),
-            Center(
-              child: Column(
+                ),
+              ),
+
+              // == SPACE ==
+              Container(
+                height: 15,
+              ),
+
+              // == ELO ==
+              Text(
+                  elo>0?"+$elo EXP":"$elo EXP",
+                      style: TextStyle(fontSize: 20),
+                ),
+
+              // == SPACE ==
+              Container(
+                height: 70,
+              ),
+
+              // == MILE TIME ==
+              Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-
-                  // == SPACE ==
-                  Container(
-                    height: 50,
+                Container(
+                  width: 300,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black38),
                   ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
 
-                  // == PLACEMENT ==
+                    children: <Widget>[
+                      Text(" Mile Time:",
+                          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                      Text("${(miletime/60).round()}:${NumberFormat("00").format(miletime%60)} ",
+                        style: TextStyle(fontSize: 22))
+                    ],
+                  ),
+                  ),
+                ],
+                ),
+
+              // == PUSH UPS ==
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
                   Container(
-                    height: 100,
-                    width: 200,
+                    width: 300,
+                    height: 50,
                     decoration: BoxDecoration(
-                      color: Colors.orangeAccent,
                       border: Border.all(color: Colors.black38),
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black26,
-                          offset: const Offset(
-                            3.0,
-                            3.0,
-                          ),
-                          blurRadius: 5,
-                          spreadRadius: 2,
-                        ),
-                        BoxShadow(
-                          color: Colors.white,
-                          offset: const Offset(0.0, 0.0),
-                          blurRadius: 0.0,
-                          spreadRadius: 0.0,
-                        ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                      children: <Widget>[
+                        Text(" Push Ups:",
+                            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                        Text("$pushups ",
+                            style: TextStyle(fontSize: 22))
                       ],
                     ),
-                    child: Center(
-                      child: Text("$placement/$total",
-                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 46),
-                      ),
-                    ),
                   ),
-
-                  // == SPACE ==
-                  Container(
-                    height: 15,
-                  ),
-
-                  // == ELO ==
-                  Text(
-                      elo>0?"+$elo EXP":"$elo EXP",
-                          style: TextStyle(fontSize: 20),
-                    ),
-
-                  // == SPACE ==
-                  Container(
-                    height: 70,
-                  ),
-
-                  // == MILE TIME ==
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                    Container(
-                      width: 300,
-                      height: 50,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black38),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-                        children: <Widget>[
-                          Text(" Mile Time:",
-                              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-                          Text("${(miletime/60).round()}:${NumberFormat("00").format(miletime%60)} ",
-                            style: TextStyle(fontSize: 22))
-                        ],
-                      ),
-                      ),
-                    ],
-                    ),
-
-                  // == PUSH UPS ==
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 300,
-                        height: 50,
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black38),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-                          children: <Widget>[
-                            Text(" Push Ups:",
-                                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-                            Text("$pushups ",
-                                style: TextStyle(fontSize: 22))
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-
-                  // == CRUNCHES ==
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 300,
-                        height: 50,
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black38),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-                          children: <Widget>[
-                            Text(" Crunches:",
-                                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-                            Text("$crunches ",
-                                style: TextStyle(fontSize: 22))
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-
-                  // == SPACE ==
-                  Container(
-                    height: 55,
-                  ),
-
-                  // == HOME BUTTON ==
-                  Container(
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.orangeAccent,
-                        ),
-                        child: Text('OK',
-                          style: TextStyle(color: Colors.black),
-                        ),
-                        onPressed: () {
-                          print('Link to home');
-                        },
-                      )
-                  ),
-
-
                 ],
+              ),
 
-            )
+              // == CRUNCHES ==
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 300,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black38),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
 
-      ),
-          ],
+                      children: <Widget>[
+                        Text(" Crunches:",
+                            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                        Text("$crunches ",
+                            style: TextStyle(fontSize: 22))
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+
+              // == SPACE ==
+              Container(
+                height: 55,
+              ),
+
+              // == HOME BUTTON ==
+              Container(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.orangeAccent,
+                    ),
+                    child: Text('OK',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    onPressed: () {
+                      print('Link to home');
+                    },
+                  )
+              ),
+
+
+            ],
+
         )
+
+      )
     );
   }
 }
