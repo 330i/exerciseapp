9c348d67830aba4fb54d3fcc66be20c920de20c8
micha <michael@mohn.com>
2021-04-10 14:08:59 -0500

    Tourney App UI finished

--- lib/pages/tournament_page.dart
+++ lib/pages/tournament_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:exerciseapp/widgets/opponent_listView.dart';
+import 'dart:ui';
 
 class TournamentPage extends StatefulWidget {
   @override
@@ -7,23 +8,89 @@ class TournamentPage extends StatefulWidget {
 }
 
 class _TournamentPageState extends State<TournamentPage> {
+  TextEditingController mileTime = new TextEditingController();
+  TextEditingController pushup = new TextEditingController();
+  TextEditingController crunches = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.white,
-        child: Column(
-          children: [
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              height: 300,
-              child: OpponentsListView()
-            )
-          ],
-        ),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Tournament Tracker",
+            style: TextStyle(
+                color: Colors.orangeAccent,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic)),
+        backgroundColor: Colors.white,
       ),
+      body: ListView(children: [
+        Container(
+          color: Colors.white,
+          child: Column(
+            children: [
+              Container(height: 300, child: OpponentsListView()),
+              Column(
+                children: [
+                  Row(children: [
+                    SizedBox(
+                      width: 20,
+                    ),
+                    _entryField("Submit a mile time", mileTime),
+                    SizedBox(width: 30,),
+                    ElevatedButton.icon(onPressed: null, icon: Icon(Icons.add), label: Text(''))
+                  ]),
+                  Row(children: [
+                    SizedBox(
+                      width: 20,
+                    ),
+                    _entryField("Submit a pushup count", pushup),
+                    SizedBox(width: 30,),
+                    ElevatedButton.icon(onPressed: null, icon: Icon(Icons.add), label: Text(''))
+                  ]),
+                  Row(children: [
+                    SizedBox(
+                      width: 20,
+                    ),
+                    _entryField("Submit a crunch count", crunches),
+                    SizedBox(width: 30,),
+                    ElevatedButton.icon(onPressed: null, icon: Icon(Icons.add), label: Text(''))
+                  ])
+                ],
+              ),
+            ],
+          ),
+        ),
+      ]),
     );
   }
+}
+
+Widget _entryField(String title, TextEditingController controller) {
+  return Container(
+    height: 110,
+    width: 250,
+    margin: EdgeInsets.symmetric(vertical: 10),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: 18,
+            color: Colors.orange,
+            fontStyle: FontStyle.italic
+          ),
+        ),
+        SizedBox(height: 10),
+        TextField(
+            controller: controller,
+            decoration: InputDecoration(
+                fillColor: Colors.orange[100],
+                border: InputBorder.none,
+                filled: true))
+      ],
+    ),
+  );
 }
