6b2dcd511f3f89b901b70496ae4041a55f2eca81
innub <emobunnyants@gmail.com>
2021-04-11 08:13:39 -0500

    init commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:exerciseapp/pages/user_profile_page.dart';
+  
+import 'package:exerciseapp/pages/login_page.dart';
 import 'package:flutter/material.dart';
-import './widgets/bottom_bar.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,10 +12,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.red),
-      home: BottomBar(),
-      // home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: FutureBuilder(
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            print(snapshot.error.toString());
+            return Container(
+              color: Colors.red,
+            );
+          }
+
+          // Once complete, show your application
+          if (snapshot.connectionState == ConnectionState.done) {
+            return LoginPage();
+          }
+
+          // Otherwise, show something whilst waiting for initialization to complete
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
     );
   }
 }
